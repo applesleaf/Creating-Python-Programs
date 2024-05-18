526260baca7706fd23dca70bdc8d5ceff2134750
Thang Sian Kop <126444266+applesleaf@users.noreply.github.com>
2024-05-18 13:53:27 -0400

    Add files via upload

---
+++ CT1.py
@@ -0,0 +1,14 @@
+import math
+
+num1 = int(input('Enter num1 : '))
+num2 = int(input('Enter num2 : '))
+
+totalNum = num1 + num2
+subtractNum = num1 - num2
+multiplyNum = num1 * num2
+divideNum = num1/num2
+
+print('Total num is ', totalNum)
+print('Subtract num is ', subtractNum)
+print('Multiply num is ', multiplyNum)
+print('Divide num is ', divideNum)
